ec2a44bae4ae36366387a269a9af347bbb9aa3ca
FabianFox <fabianguelzau@web.de>
2019-08-01 16:15:34 +0200

    solved issue with number of elements

--- code/UNSC_scraper_voting.R
+++ code/UNSC_scraper_voting.R
@@ -7,20 +7,21 @@ p_load(tidyverse, rvest, janitor)
 
 # Base df for iteration
 unsc_voting <- tibble(
-  url = paste0("https://digitallibrary.un.org/search?ln=en&rg=100&jrec=", 
-               seq(1, 2480, 100), 
-               "&fct__2=Security+Council&fct__2=Security+Council&cc=Voting+Data&sf=year")
+  url = paste0(
+    "https://digitallibrary.un.org/search?ln=en&rg=100&jrec=",
+    seq(1, 2480, 100),
+    "&fct__2=Security+Council&fct__2=Security+Council&cc=Voting+Data&sf=year"
+  )
 )
 
 # Locate links to voting tables
 # Note: Apparently, there is no easy way to get the href-attribute
-extract_vote <- function(x){
-  
+extract_vote <- function(x) {
   x %>%
     read_html() %>%
     html_nodes(".pagebodystripemiddle a") %>%
     html_attr("href") %>%
-    .[str_detect(., "record") & !is.na(.)]     # filter relevant links
+    .[str_detect(., "record") & !is.na(.)] # filter relevant links
 }
 
 # Safely
@@ -29,21 +30,21 @@ safely_extract_vote <- safely(extract_vote, otherwise = NULL)
 # Get the links to the tables
 unsc_voting <- unsc_voting %>%
   mutate(voting_table = map(
-    url, ~ safely_extract_vote(.x)))
+    url, ~ safely_extract_vote(.x)
+  ))
 
 # Clean URLs and unnest
 unsc_voting <- unsc_voting %>%
   mutate(voting_table = map(voting_table, 1)) %>%
   unnest() %>%
   mutate(voting_table = paste0("https://digitallibrary.un.org", voting_table))
-           
+
 # Extract information from tables
 
 # Function
-table_fun <- function(x){
-  
-  table <- read_html(x) 
-  
+table_fun <- function(x) {
+  table <- read_html(x)
+
   col1 <- table %>%
     html_nodes("div.metadata-row > span:nth-child(1)") %>%
     html_text()
@@ -51,9 +52,11 @@ table_fun <- function(x){
   col2 <- table %>%
     html_nodes("div.metadata-row > span:nth-child(2)") %>%
     html_text()
-  
-  df <- tibble(variable = col1, 
-               value = col2)
+
+  df <- tibble(
+    variable = col1,
+    value = col2
+  )
 }
 
 # Safely
@@ -72,21 +75,29 @@ unsc_voting <- readRDS(file = "./output/UNSC_voting_data_raw.rds")
 # Data cleaning
 # Lift resolution-id from listed df to top level
 unsc_voting <- unsc_voting %>%
-  mutate(id = pluck(voting_df, "value"),
-         id = pluck(id, 3))
-           
+  mutate(
+    id = pluck(voting_df, "value"),
+    id = pluck(id, 3)
+  )
+
 # Clean the listed table of information on the resolutions
 unsc_voting <- unsc_voting %>%
-  mutate(voting_df = map(voting_df, 
-                         ~ spread(., key = variable, value = value) %>%
-                           clean_names() %>% 
-                           mutate(., vote = paste0(" ", vote))))
+  mutate(voting_df = map(
+    voting_df,
+    ~ spread(., key = variable, value = value) %>%
+      clean_names() %>%
+      mutate(., vote = paste0(" ", vote))
+  ))
 
 # Disentangle country/vote
 unsc_voting <- unsc_voting %>%
-  mutate(voting_df = map(voting_df, 
-                         ~ mutate(., country = str_split(vote, "(\\sY\\s|\\sN\\s|\\sA\\s)"),
-                                  vote_split = str_extract_all(vote, "(\\sY\\s|\\sN\\s|\\sA\\s)"))))
+  mutate(voting_df = map(
+    voting_df,
+    ~ mutate(.,
+      country = str_split(vote, "(\\sY\\s|\\sN\\s|\\sA\\s)"),
+      vote_split = str_extract_all(vote, "(\\sY\\s|\\sN\\s|\\sA\\s)")
+    )
+  ))
 
 # Rewrite as map
 for (i in seq_along(unsc_voting$voting_df)) {
@@ -96,27 +107,38 @@ for (i in seq_along(unsc_voting$voting_df)) {
 
 # Check particular values where number of votes != number of countries
 unsc_voting <- unsc_voting %>%
-  mutate(length_country = 
-           map_int(unsc_voting$voting_df, ~lengths(.x$country)),
-         length_vote_split = 
-         map_int(unsc_voting$voting_df, ~lengths(.x$vote_split))
+  mutate(
+    length_country =
+      map_int(unsc_voting$voting_df, ~ lengths(.x$country)),
+    length_vote_split =
+      map_int(unsc_voting$voting_df, ~ lengths(.x$vote_split))
   ) %>%
   mutate(res_voted_on = length_country == length_vote_split) %>%
   select(-length_country, -length_vote_split)
 
-# Remove resolutions w/o vote
+# countries with comma in their name cause troubles, i.e. "GERMANY, FEDERAL REPUBLIC OF"
+# str_replace them:
 unsc_voting <- unsc_voting %>%
-  filter(res_voted_on == TRUE) 
+  mutate(voting_df = map(voting_df, ~.x %>%
+    mutate(
+      country = str_replace_all(
+        country, "GERMANY, FEDERAL REPUBLIC OF", "FEDERAL REPUBLIC OF GERMANY"
+      )
+    )))
 
-# Error: All nested columns must have the same number of elements.
-# not due to different number of member states and votes
-# Might run on each data set and then back combine...
+# separate member's vote and country
 unsc_voting <- unsc_voting %>%
-  mutate(voting_df = map(voting_df, 
-                        ~ separate_rows(., country, vote_split, sep = ",")))
-                        
+  mutate(voting_df = map(
+    voting_df,
+    ~ separate_rows(., country, vote_split, sep = ",")
+  ))
+
+# Clean the strings
 unsc_voting <- unsc_voting %>%
-  mutate(voting_df = map(voting_df, 
-                         ~ mutate(., country = str_trim(str_remove_all(country, "([:lower:]|[:punct:])"), "left"),
-                                  vote = str_trim(str_remove_all(vote_split, "([:lower:]|[:punct:])"), "both"))))
-         
+  mutate(voting_df = map(
+    voting_df,
+    ~ mutate(.,
+      country = str_trim(str_remove_all(country, "([:lower:]|[:punct:])"), "left"),
+      vote = str_trim(str_remove_all(vote_split, "([:lower:]|[:punct:])"), "both")
+    )
+  ))
