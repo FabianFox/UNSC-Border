71e6d5c40024312eb738971897874f8d8f4a6083
FabianFox <fabianguelzau@web.de>
2019-08-20 17:22:25 +0200

    text cleaning

--- code/UNSC_preprocessing.R
+++ code/UNSC_preprocessing.R
@@ -24,9 +24,12 @@ res_meta <- readRDS("./output/UNSC_voting_data.rds")
 ### ------------------------------------------------------------------------ ###
 # Filter to resolutions containing "border"
 res_files <- res_files %>%
-  mutate(border = str_detect(text, "border")) %>%
+  mutate(border = str_detect(text, "border"),
+         year = strtoi(stringi::stri_extract_last_regex(doc_id, "[:digit:]{4}")),
+         text = str_replace_all(text, "(\\\\r?\\\\n)|(\\\\n)", " "),               # replace carriage return (\r) and line feed (\n)
+         text = str_replace_all(text, "\\s+", " ")) %>%                            # delete excessive spaces
   filter(border == TRUE) %>%
-  select(-border)
+  select(-border) 
 
 # Transform into a quanteda corpus
 res_qcorpus <- corpus(res_files,
@@ -35,4 +38,12 @@ res_qcorpus <- corpus(res_files,
 
 # Transfrom to quanteda document-feature matrix
 res_dfm <- dfm(res_qcorpus, tolower = TRUE, stem = TRUE, 
-               remove_punct = TRUE, remove = stopwords("english"))
+               remove_punct = TRUE, remove = stopwords("english"),
+               remove_numbers = TRUE)
+
+# Basic analysis
+### ------------------------------------------------------------------------ ###
+
+# TF-IDF
+res_tf <- dfm_tfidf(res_dfm, base = 10)
+topfeatures(res_tf[15, ], n = 10)
