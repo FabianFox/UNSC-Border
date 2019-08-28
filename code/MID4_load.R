dbc6d97f6b203ef87a5e7767b9b2f394ec7ffa7e
FabianFox <fabianguelzau@web.de>
2019-08-28 16:48:34 +0200

    join incident/dyad

--- code/MID4_load.R
+++ code/MID4_load.R
@@ -6,21 +6,43 @@
 # Load/install packages
 ### ------------------------------------------------------------------------ ###
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, rio)
+p_load(tidyverse, rio, countrycode)
 
-# Load MIDA 4.3
+# Load MID 4.3: A (incident level) and B (dyad level)
 ### ------------------------------------------------------------------------ ###
 # One record per dispute
 mid_inc.df <- import("./data/independent variables/MID 4.3/MIDA 4.3.csv") %>%
-  filter(styear >= 1945)
+  filter(between(styear, 1945, 2010)) 
 
 # One record per participant per dispute
 mid_dyad.df <- import("./data/independent variables/MID 4.3/MIDB 4.3.csv") %>%
-  filter(styear >= 1945)
+  filter(between(styear, 1945, 2010)) %>%
+  mutate(cname = countrycode(stabb, "cowc", "cow.name", 
+                             custom_match = c("GFR" = "German Federal Republic",
+                                              "RUM" = "Romania",
+                                              "USR" = "Russia",
+                                              "VTM" = "Vietnam",
+                                              "ZAI" = "Democratic Republic of the Congo")))
 
-# Number of MIDs since 1945
-mid.df %>%
-  filter(styear >= 1945) %>%
-  count(styear) %>%
-  ggplot(aes(styear, n)) +
-  geom_bar(stat = "identity")
+# Join conflict parties to incident level data
+### ------------------------------------------------------------------------ ###
+mid_dyad.df <- mid_dyad.df %>%
+  group_by(dispnum3, sidea) %>%
+  summarise(cntrya = paste(cname, collapse = ", "))
+
+# Separate side A and B
+sideA <- mid_dyad.df %>%
+  filter(sidea == 1)
+
+sideB <- mid_dyad.df %>%
+  filter(sidea == 0) %>%
+  select(-sidea) %>%
+  rename("cntryb" = "cntrya")
+
+# Merge again into separate columns (one row per conflict)
+mid_dyad.df <- sideA %>%
+  select(dispnum3, cntrya) %>%
+  left_join(sideB)
+
+# Checks
+which(!(sideA$dispnum3 %in% sideB$dispnum3)) # col: 25, dispnum3: 258
